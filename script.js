a9469dec2bc3c6fad725941ba0cf207dd55c0e0d
dantvi <daniel.tving@gmail.com>
2024-10-06 15:50:14 +0200

    form validation and page functionality

--- script.js
+++ script.js
@@ -1,11 +1,59 @@
+const signUpFormEl = document.getElementById('sign-up-form');
 const form = document.getElementById('form');
-const email = document.getElementById('email-input-field');
-const button = document.getElementById('submit-button');
+const userInputEl = document.getElementById('email-input-field');
+const userEmailEl = document.getElementById('userEmail');
+const dismissButton = document.getElementById('dismiss-button');
 const errorMessage = document.getElementById('error-message');
 const successMessage = document.getElementById('success-message');
 
+let showingErrorMessage = false;
+let userEmail = '';
 
-// Prevent default behaviour for the form
-form.addEventListener('submit', (e) => {
+function validateEmail(email) {
+  userEmail = email;
+  const validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  if (email.match(validRegex)) {
+    // Set User Email to Success Page
+    userEmailEl.textContent = userEmail;
+    // Show Success Page
+    successMessage.classList.remove('hidden');
+    // Hide Sign Up Form
+    signUpFormEl.classList.add('hidden');
+    if (showingErrorMessage) {
+      errorMessage.classList.add('hidden');
+      showingErrorMessage = false;
+    }
+  } else {
+    // Show error message
+    errorMessage.classList.remove('hidden');
+    showingErrorMessage = true;
+  }
+}
+
+const validateForm = () => {
+  const inputValue = userInputEl.value;
+  // If input field is empty
+  if (inputValue === '') {
+    // Show error message
+    errorMessage.classList.remove('hidden');
+    showingErrorMessage = true;
+    return;
+  }
+  validateEmail(inputValue);
+  userInputEl.value = '';
+}
+
+const processFormData = (e) => {
   e.preventDefault();
-});
+  validateForm();
+}
+
+const showLandingPage = () => {
+  // Show Sign Up Form
+  signUpFormEl.classList.remove('hidden');
+  // Hide Success Page
+  successMessage.classList.add('hidden');
+}
+
+form.addEventListener('submit', processFormData);
+dismissButton.addEventListener('click', showLandingPage);
